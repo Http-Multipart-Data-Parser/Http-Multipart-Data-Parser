bc446a794d3250fe3aded362a977f936339fe768
Woods, Jake <Jake.Woods@macgen.com.au>
2013-02-04 10:22:44 +1100

    Updated assembly copyright tag

--- HttpMultipartParserUnitTest/Properties/AssemblyInfo.cs
+++ HttpMultipartParserUnitTest/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HttpMultipartParserUnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HttpMultipartParserUnitTest")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright © Jake Woods 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

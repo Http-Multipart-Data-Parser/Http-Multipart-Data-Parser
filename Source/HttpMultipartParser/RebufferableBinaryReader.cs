e5b4f3d64e0d848917a778463deb3ab9dcabd19d
Jericho <desautelsj@hotmail.com>
2019-11-17 17:09:55 -0500

    Add async methods in RebufferableBinaryReader

--- Source/HttpMultipartParser/RebufferableBinaryReader.cs
+++ Source/HttpMultipartParser/RebufferableBinaryReader.cs
@@ -27,6 +27,8 @@
 
 using System.IO;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HttpMultipartParser
 {
@@ -281,6 +283,165 @@ namespace HttpMultipartParser
             return data == null ? null : encoding.GetString(data);
         }
 
+        /// <summary>
+        ///     Asynchronously reads a single byte as an integer from the stream.
+        ///     Returns -1 if no data is left to read.
+        /// </summary>
+        /// <param name="cancellationToken">
+        ///     The cancellation token.
+        /// </param>
+        /// <returns>
+        ///     The <see cref="byte" /> that was read.
+        /// </returns>
+        public async Task<int> ReadAsync(CancellationToken cancellationToken = default)
+        {
+            int value = -1;
+            while (value == -1)
+            {
+                if (!streamStack.HasData())
+                {
+                    if (await StreamDataAsync(cancellationToken).ConfigureAwait(false) == 0)
+                    {
+                        return -1;
+                    }
+                }
+
+                value = streamStack.Read();
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        ///     Asynchronously reads the specified number of bytes from the stream, starting from a
+        ///     specified point in the byte array.
+        /// </summary>
+        /// <param name="buffer">
+        ///     The buffer to read data into.
+        /// </param>
+        /// <param name="index">
+        ///     The index of buffer to start reading into.
+        /// </param>
+        /// <param name="count">
+        ///     The number of bytes to read into the buffer.
+        /// </param>
+        /// <param name="cancellationToken">
+        ///     The cancellation token.
+        /// </param>
+        /// <returns>
+        ///     The number of bytes read into buffer. This might be less than the number of bytes requested if that many bytes are not available,
+        ///     or it might be zero if the end of the stream is reached.
+        /// </returns>
+        public async Task<int> ReadAsync(byte[] buffer, int index, int count, CancellationToken cancellationToken = default)
+        {
+            int amountRead = 0;
+            while (amountRead < count)
+            {
+                if (!streamStack.HasData())
+                {
+                    if (await StreamDataAsync(cancellationToken).ConfigureAwait(false) == 0)
+                    {
+                        return amountRead;
+                    }
+                }
+
+                amountRead += streamStack.Read(buffer, index + amountRead, count - amountRead);
+            }
+
+            return amountRead;
+        }
+
+        /// <summary>
+        ///     Asynchronously reads the specified number of characters from the stream, starting from a
+        ///     specified point in the byte array.
+        /// </summary>
+        /// <param name="buffer">
+        ///     The buffer to read data into.
+        /// </param>
+        /// <param name="index">
+        ///     The index of buffer to start reading into.
+        /// </param>
+        /// <param name="count">
+        ///     The number of characters to read into the buffer.
+        /// </param>
+        /// <param name="cancellationToken">
+        ///     The cancellation token.
+        /// </param>
+        /// <returns>
+        ///     The number of characters read into buffer. This might be less than the number of
+        ///     characters requested if that many characters are not available,
+        ///     or it might be zero if the end of the stream is reached.
+        /// </returns>
+        public async Task<int> ReadAsync(char[] buffer, int index, int count, CancellationToken cancellationToken = default)
+        {
+            int amountRead = 0;
+            while (amountRead < count)
+            {
+                if (!streamStack.HasData())
+                {
+                    if (await StreamDataAsync(cancellationToken).ConfigureAwait(false) == 0)
+                    {
+                        return amountRead;
+                    }
+                }
+
+                amountRead += streamStack.Read(buffer, index + amountRead, count - amountRead);
+            }
+
+            return amountRead;
+        }
+
+        /// <summary>
+        ///     Asynchronously reads a series of bytes delimited by the byte encoding of newline for this platform.
+        ///     The newline bytes will not be included in the return data.
+        /// </summary>
+        /// <param name="cancellationToken">
+        ///     The cancellation token.
+        /// </param>
+        /// <returns>
+        ///     A byte array containing all the data up to but not including the next newline in the stack.
+        /// </returns>
+        public async Task<byte[]> ReadByteLineAsync(CancellationToken cancellationToken = default)
+        {
+            using (var builder = new MemoryStream())
+            {
+                while (true)
+                {
+                    if (!streamStack.HasData())
+                    {
+                        if (await StreamDataAsync(cancellationToken).ConfigureAwait(false) == 0)
+                        {
+                            return builder.Length > 0 ? builder.ToArray() : null;
+                        }
+                    }
+
+                    byte[] line = streamStack.ReadByteLine(out bool hitStreamEnd);
+
+                    await builder.WriteAsync(line, 0, line.Length, cancellationToken).ConfigureAwait(false);
+                    if (!hitStreamEnd)
+                    {
+                        return builder.ToArray();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Asynchronously reads a line from the stack delimited by the newline for this platform. The newline
+        ///     characters will not be included in the stream.
+        /// </summary>
+        /// <param name="cancellationToken">
+        ///     The cancellation token.
+        /// </param>
+        /// <returns>
+        ///     The <see cref="string" /> containing the line or null if end of stream.
+        /// </returns>
+        public async Task<string> ReadLineAsync(CancellationToken cancellationToken = default)
+        {
+            byte[] data = await ReadByteLineAsync(cancellationToken).ConfigureAwait(false);
+            return data == null ? null : encoding.GetString(data);
+        }
+
         #endregion
 
         #region Methods
@@ -321,6 +482,41 @@ namespace HttpMultipartParser
             var buffer = new byte[bufferSize];
             int amountRead = stream.Read(buffer, 0, buffer.Length);
 
+            PushToStack(buffer, amountRead);
+
+            return amountRead;
+        }
+
+        /// <summary>
+        ///     Asynchronously reads more data from the stream into the stream stack.
+        /// </summary>
+        /// <param name="cancellationToken">
+        ///     The cancellation token.
+        /// </param>
+        /// <returns>
+        ///     The number of bytes read into the stream stack as an <see cref="int" />.
+        /// </returns>
+        private async Task<int> StreamDataAsync(CancellationToken cancellationToken = default)
+        {
+            var buffer = new byte[bufferSize];
+            int amountRead = await stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false);
+
+            PushToStack(buffer, amountRead);
+
+            return amountRead;
+        }
+
+        /// <summary>
+        ///     Push the data read from the stram into the stream stack.
+        /// </summary>
+        /// <param name="buffer">
+        ///     The data that was read from the stream.
+        /// </param>
+        /// <param name="amountRead">
+        ///     The number of bytes read from the stream.
+        /// </param>
+        private void PushToStack(byte[] buffer, int amountRead)
+        {
             // We need to check if our stream is using our encodings
             // BOM, if it is we need to jump it.
             int bomOffset = GetBomOffset(buffer);
@@ -340,8 +536,6 @@ namespace HttpMultipartParser
                     streamStack.Push(buffer);
                 }
             }
-
-            return amountRead;
         }
 
         #endregion

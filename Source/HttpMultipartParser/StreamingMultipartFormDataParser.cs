dc68127ed479ef7d40a49e7fac741861cfd5eb10
Jericho <desautelsj@hotmail.com>
2021-01-03 12:15:30 -0500

    (GH-93) Add interface for StreamingMultipartFormDataParser

--- Source/HttpMultipartParser/StreamingMultipartFormDataParser.cs
+++ Source/HttpMultipartParser/StreamingMultipartFormDataParser.cs
@@ -41,7 +41,7 @@ namespace HttpMultipartParser
     ///       parser.Run();
     ///   </code>
     /// </example>
-    public class StreamingMultipartFormDataParser
+    public class StreamingMultipartFormDataParser : IStreamingMultipartFormDataParser
     {
         #region Constants
 
@@ -161,6 +161,8 @@ namespace HttpMultipartParser
 
         #endregion
 
+        #region Public Methods
+
         /// <summary>
         ///     Begins executing the parser. This should be called after all handlers have been set.
         /// </summary>
@@ -229,35 +231,9 @@ namespace HttpMultipartParser
             await ParseAsync(reader, cancellationToken).ConfigureAwait(false);
         }
 
+        #endregion
+
         #region Public Properties
-
-        /// <summary>
-        /// The FileStreamDelegate defining functions that can handle file stream data from this parser.
-        ///
-        /// Delegates can assume that the data is sequential i.e. the data received by any delegates will be
-        /// the data immediately following any previously received data.
-        /// </summary>
-        /// <param name="name">The name of the multipart data.</param>
-        /// <param name="fileName">The name of the file.</param>
-        /// <param name="contentType">The content type of the multipart data.</param>
-        /// <param name="contentDisposition">The content disposition of the multipart data.</param>
-        /// <param name="buffer">Some of the data from the file (not necessarily all of the data).</param>
-        /// <param name="bytes">The length of data in buffer.</param>
-        /// <param name="partNumber">Each chunk (or "part") in a given file is sequentially numbered, starting at zero.</param>
-        /// <param name="additionalProperties">Properties other than the "well known" ones (such as name, filename, content-type, etc.) associated with a file stream.</param>
-        public delegate void FileStreamDelegate(
-            string name, string fileName, string contentType, string contentDisposition, byte[] buffer, int bytes, int partNumber, IDictionary<string, string> additionalProperties);
-
-        /// <summary>
-        /// The StreamClosedDelegate defining functions that can handle stream being closed.
-        /// </summary>
-        public delegate void StreamClosedDelegate();
-
-        /// <summary>
-        /// The ParameterDelegate defining functions that can handle multipart parameter data.
-        /// </summary>
-        /// <param name="part">The parsed parameter part.</param>
-        public delegate void ParameterDelegate(ParameterPart part);
 
         /// <summary>
         ///     Gets or sets the binary buffer size.
@@ -265,7 +241,7 @@ namespace HttpMultipartParser
         public int BinaryBufferSize { get; set; }
 
         /// <summary>
-        ///     Gets the encoding.
+        /// Gets the encoding.
         /// </summary>
         public Encoding Encoding { get; private set; }
 

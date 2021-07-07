710eba0574312acc040d86acf142b1a45fc0db74
Jericho <desautelsj@hotmail.com>
2021-07-07 12:17:31 -0400

    (GH-98) Unit test to confirm the bug reported in GitHub issue

--- Source/HttpMultipartParser.UnitTests/SubsequenceFinderUnitTests.cs
+++ Source/HttpMultipartParser.UnitTests/SubsequenceFinderUnitTests.cs
@@ -15,5 +15,21 @@ namespace HttpMultipartParser.UnitTests
 
             Assert.Equal(2, SubsequenceFinder.Search(A, B, A.Length));
         }
+
+        [Fact]
+        // This unit test demonstrates the bug in the 'Search' method when the haystack
+        // contains a subset of the needle. Specifically, notice the '0x0D' byte in the
+        // sample haystack followed by '0x0D, 0x0A' (which corresponds to the needle). 
+        // See: https://github.com/Http-Multipart-Data-Parser/Http-Multipart-Data-Parser/issues/98
+        public void Haystack_contains_subset_of_needle()
+        {
+            var haystack = new byte[] { 0x96, 0xC7, 0x0D, 0x0D, 0x0A, 0x2D, 0x2D, 0x63, 0x65 };
+            var needle = new byte[] { 0x0D, 0x0A };
+            var expectedPosition = 3;
+
+            var result = SubsequenceFinder.Search(haystack, needle);
+
+            Assert.Equal(expectedPosition, result);
+        }
     }
 }

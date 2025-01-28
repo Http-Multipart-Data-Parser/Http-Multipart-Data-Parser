9ad5bb66639a8e98f4bd4e4cae16280a502e9bfc
Jericho <desautelsj@hotmail.com>
2025-01-28 12:03:37 -0500

    (GH-123) Unit test to demonstrate bug

---
+++ Source/HttpMultipartParser.UnitTests/ParserScenarios/BoundaryEndsWithDoubleDash.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HttpMultipartParser.UnitTests.ParserScenarios
+{
+	public class BoundaryEndsWithDoubleDash
+	{
+		// The boundary in this scenario ends with '--'. This is an unusual scenario but perfectly legitimate.
+		// For details, see: https://github.com/Http-Multipart-Data-Parser/Http-Multipart-Data-Parser/issues/123
+		private static readonly string _testData = TestUtil.TrimAllLines(
+			@"--boundary_text--
+			Content-type: text/plain; charset=UTF-8
+			Content-Disposition: form-data; name=""file1""; filename=""file1.txt""
+
+			file content here
+			--boundary_text--
+			Content-type: text/plain; charset=UTF-8
+			Content-Disposition: form-data; name=""file2""; filename=""file2.txt""
+
+			file content here 2
+			--boundary_text----"
+		);
+
+		private static readonly TestData _testCase = new TestData(
+			_testData,
+			Enumerable.Empty<ParameterPart>().ToList(),
+			new List<FilePart>() {
+				new FilePart("file1", "file1.txt", TestUtil.StringToStreamNoBom("file content here")),
+				new FilePart("file2", "file2.txt", TestUtil.StringToStreamNoBom("file content here 2")),
+			}
+		);
+
+		public BoundaryEndsWithDoubleDash()
+		{
+			foreach (var filePart in _testCase.ExpectedFileData)
+			{
+				filePart.Data.Position = 0;
+			}
+		}
+
+		/// <summary>
+		///     Tests for correct detection of the boundary in the input stream.
+		/// </summary>
+		[Fact]
+		public void CanAutoDetectBoundary()
+		{
+			using (Stream stream = TestUtil.StringToStream(_testCase.Request, Encoding.UTF8))
+			{
+				var parser = MultipartFormDataParser.Parse(stream);
+				Assert.True(_testCase.Validate(parser));
+			}
+		}
+
+		/// <summary>
+		///     Tests for correct detection of the boundary in the input stream.
+		/// </summary>
+		[Fact]
+		public async Task CanAutoDetectBoundaryAsync()
+		{
+			using (Stream stream = TestUtil.StringToStream(_testCase.Request, Encoding.UTF8))
+			{
+				var parser = await MultipartFormDataParser.ParseAsync(stream, Encoding.UTF8);
+				Assert.True(_testCase.Validate(parser));
+			}
+		}
+	}
+}
